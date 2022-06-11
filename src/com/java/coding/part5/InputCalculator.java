90e1926a49376e5b4b9a25d96a1d2702930c05d7
Abhilash <abhilashjanarrdhan@gmail.com>
2022-06-11 16:04:16 +0530

    Uploaded InputCalculator Program

---
+++ src/com/java/coding/part5/InputCalculator.java
@@ -0,0 +1,26 @@
+package com.java.coding.part5;
+
+import java.util.Scanner;
+
+public class InputCalculator {
+
+    public static void inputThenPrintSumAndAverage() {
+
+        Scanner scan = new Scanner(System.in);
+        int count = 0, sum = 0;
+        while (scan.hasNextInt()) {
+            int number = scan.nextInt();
+            sum += Integer.parseInt(number + "");
+            count++;
+        }
+        System.out.println("SUM = " + sum + " AVG = " + Math.round((float)sum/count));
+
+    }
+
+    public static void main(String[] args) {
+
+        inputThenPrintSumAndAverage();
+
+    }
+
+}
