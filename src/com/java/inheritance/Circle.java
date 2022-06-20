dc78b24e168843bd385368248f76c2089bf0a7fc
Abhilash <abhilashjanarrdhan@gmail.com>
2022-06-20 18:01:11 +0530

    Uploaded OOP's - Inheritance 2 program CCMain - Circle Cylinder RC Main - Rectangle Cuboid

---
+++ src/com/java/inheritance/Circle.java
@@ -0,0 +1,20 @@
+package com.java.inheritance;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        if (radius >= 0.0)
+            this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+}
