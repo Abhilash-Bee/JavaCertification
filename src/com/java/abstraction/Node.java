65584e1f4c7b3ae7de7a7e07aece6049db7752a7
Abhilash <abhilashjanarrdhan@gmail.com>
2022-06-29 14:21:24 +0530

    Uploaded Abstraction -> ListItem, MyLinkedList, Node, NodeList, SearchTree program

---
+++ src/com/java/abstraction/Node.java
@@ -0,0 +1,39 @@
+package com.java.abstraction;
+
+public class Node extends ListItem {
+
+
+    public Node(Object value) {
+        super(value);
+    }
+
+    @Override
+    ListItem next() {
+        return this.rightLink;
+    }
+
+    @Override
+    ListItem setNext(ListItem link) {
+        this.rightLink = link;
+        return this.rightLink;
+    }
+
+    @Override
+    ListItem previous() {
+        return this.leftLink;
+    }
+
+    @Override
+    ListItem setPrevious(ListItem link) {
+        this.leftLink = link;
+        return this.leftLink;
+    }
+
+    @Override
+    int compareTo(ListItem item) {
+        if (item != null)
+            return ((String) super.getValue()).compareTo((String) item.getValue());
+        else
+            return -1;
+    }
+}
